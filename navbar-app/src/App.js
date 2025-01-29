50a702fa62f850296c397e2a2f2061217ebe8235
Jamesmbaya <chinkupozee@gmail.com>
2025-01-29 10:30:35 -0800

    new changes

--- navbar-app/src/App.js
+++ navbar-app/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import React, { useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
 
   const [theme, setTheme] = useState('light');
-  
+
   return (
-    <div className="container">
-      <Navbar/>
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
 
-export default App
+export default App;
